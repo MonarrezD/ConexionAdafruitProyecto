ba61443be2bfefd66e2343967ac3895ddae0b80b
Victor <vamonarrez21@gmail.com>
2023-12-02 18:38:11 -0600

    Correcion De La Llave En Adafruit Y Agregado Codigo Del Sensor Fotorresistencia

---
+++ conexionadafruit2/config.h
@@ -0,0 +1,9 @@
+#define IO_USERNAME  "victor009"
+#define IO_KEY       "aio_CaRy32K7UlgXAiHXWrCSww9tqPUf"
+
+#define WIFI_SSID "INFINITUM3C47"
+#define WIFI_PASS "FU5dUKWkpN"
+
+#include "AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
